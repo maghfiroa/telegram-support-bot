57ef4ce5101464ff7632667fb95968cd487486ee
Jmickhwal <95651774+Jmickhwal@users.noreply.github.com>
2021-12-27 23:18:24 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -156,6 +156,7 @@ def forward_to_user(update, context):
 
 
 def setup_dispatcher(dp):
+    dp.add_handler(CommandHandler('broadcast', broadcast))
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('vip', vip))
     dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
