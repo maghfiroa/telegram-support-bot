638389826779b7cd11dd9cb5ff0493fb175c0814
Jmickhwal <95651774+Jmickhwal@users.noreply.github.com>
2021-12-27 23:49:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 # Setup bot handlers
 updater = Updater(TELEGRAM_TOKEN)
 
-dp = updater.dispatcher
-dp = setup_dispatcher(dp)
+dispatcher = updater.dispatcher
+dp = setup_dispatcher(dispatcher)
 
 # enable logging
 logging.basicConfig(
